1c99f63e077fcce6b55d043531e678b209ec428f
Alex Soto <asotobu@gmail.com>
2019-03-19 11:51:08 +0100

    Adds predefined output

--- src/test/java/com/lordofthejars/hoverfly/ExternalWorldClockServiceGatewayTest.java
+++ src/test/java/com/lordofthejars/hoverfly/ExternalWorldClockServiceGatewayTest.java
@@ -14,6 +14,18 @@ import org.junit.jupiter.api.extension.ExtendWith;
                              enableAutoCapture=true)
 public class ExternalWorldClockServiceGatewayTest {
 
+    private static final String OUTPUT = "{\n"
+        + "   \"$id\":\"1\",\n"
+        + "   \"currentDateTime\":\"2019-03-12T10:54+01:00\",\n"
+        + "   \"utcOffset\":\"01:00:00\",\n"
+        + "   \"isDayLightSavingsTime\":false,\n"
+        + "   \"dayOfTheWeek\":\"Tuesday\",\n"
+        + "   \"timeZoneName\":\"Central Europe Standard Time\",\n"
+        + "   \"currentFileTime\":131968616698822965,\n"
+        + "   \"ordinalDate\":\"2019-71\",\n"
+        + "   \"serviceResponse\":null\n"
+        + "}";
+
     @Test
     public void should_get_time_from_external_service() {
         
